53aa1faa7e99b97dc0d0ca9f86e5677beabdf97c
Frivcoom111 <carloseduardofaustino67@gmail.com>
2026-05-29 12:33:51 -0300

    Adicione `@types/express` ao `package.json` e atualize o `pnpm-lock.yaml`; refatore o `app.ts` para exportar a função `appBuild`.

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,2 +1,9 @@
-import express from 'express';
+import express from "express";
+
+export const appBuild = async () => {
+    const app = express();
+    app.use(express.json())
+
+    return app
+}
 
